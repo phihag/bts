d143a4e5e92781a9734974e3509b37a78d66717c
Philipp Hagemeister <phihag@phihag.de>
2017-03-21 21:50:37 +0100

    helper script to decode BTP requests/responses

---
+++ div/decode.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+'use strict';
+
+const zlib = require('zlib');
+
+function main() {
+	const argv = process.argv.slice(2);
+	if (argv.length !== 1) {
+		console.error('Usage: decode.js HEXDUMP');
+		sys.exit(1);
+	}
+	const hex = argv[0].trim();
+
+	const main_buf = new Buffer(hex, 'hex');
+	const response_buf = zlib.gunzipSync(main_buf, {});
+	const response_str = response_buf.toString('utf8');
+	console.log(response_str);
+}
+
+main();
