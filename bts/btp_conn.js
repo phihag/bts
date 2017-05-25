29078923d669a510a76d7aff531c07fd39a26f0c
Philipp Hagemeister <phihag@phihag.de>
2017-05-25 19:14:17 +0200

    typo

--- bts/btp_conn.js
+++ bts/btp_conn.js
@@ -140,8 +140,8 @@ class BTPConn {
 				}
 
 				for (const m of matches) {
-					if (typeof match.team1_won !== 'boolean') {
-						serror.silent('match ' + match._id + ' has needsync but is not over');
+					if (typeof m.team1_won !== 'boolean') {
+						serror.silent('match ' + m._id + ' has needsync but is not over');
 						continue;
 					}
 
