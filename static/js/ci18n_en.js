f6a5fa8abdbf7f67e454552e72d793a71e9de4aa
Philipp Hagemeister <phihag@phihag.de>
2018-06-09 13:23:54 +0200

    english translation

--- static/js/ci18n_en.js
+++ static/js/ci18n_en.js
@@ -4,6 +4,28 @@ var ci18n_en = {
 '_name': 'English',
 
 'Unassigned Matches': 'Unassigned Matches',
+'edit tournament': 'edit tournament',
+'Court': 'Court',
+'Match': 'Match',
+'Players': 'Players',
+'Umpire': 'Umpire',
+'State': 'State',
+'Umpire:': 'Umpire:',
+'Finished Matches': 'Finished Matches',
+'Time:': 'Time:',
+'(Singles)': '(Singles)',
+'e.g. MX O55': 'e.g. MX O55',
+'e.g. semi-finals': 'e.g. semi-finals',
+'Number:': 'Number:',
+'Cancel': 'Cancel',
+'Change': 'Change',
+'No umpire': 'No umpire',
+'Edit match': 'Edit match',
+'Back': 'Back',
+'PDF': 'PDF',
+'Print': 'Print',
+'Add Match': 'Add Match',
+' Ready to start ': ' Ready to start ',
 
 
 };
