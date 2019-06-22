e7bf62448018c01ab206b6fac8fd3842813fb5e4
Philipp Hagemeister <phihag@phihag.de>
2019-06-23 01:55:57 +0200

    use bup's country database
    Unfortunately this may complicate installation a little bit; but that's just a symptom that we need to fix anyways.
    Use bup's countrycodes database so we don't replicate it in bts.

--- bts/countries.js
+++ bts/countries.js
@@ -1,9 +1,27 @@
+const assert = require('assert');
 const serror = require('./serror');
 
-const {COUNTRIES_TABLE} = require('../static/js/countries.js');
+let COUNTRIES_TABLE;
 
 function lookup(code) {
-	var res = COUNTRIES_TABLE[code];
+	if (!COUNTRIES_TABLE) {
+		let bup_module;
+		try {
+			bup_module = require('../static/bup/dev/js/countrycodes.js');
+		} catch (e) {
+			serror.silent_once(`Failed to load country database: ${e.stack}`);
+			return code;
+		}
+		try {
+			COUNTRIES_TABLE = bup_module.TABLE;
+			assert(COUNTRIES_TABLE);
+		} catch(e) {
+			serror.silent_once(`Failed to get country name table: ${e.stack}`);
+			return code;
+		}
+	}
+
+	const res = COUNTRIES_TABLE[code];
 
 	if (!res) {
 		serror.silent('Unknown country ' + JSON.stringify(code));
