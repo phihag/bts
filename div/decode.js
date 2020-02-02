a0f51cc519770930dd284e311e6167f55bd631a1
Philipp Hagemeister <phihag@phihag.de>
2020-02-02 15:18:04 +0100

    decode.js: Also support BTP header
    BTP messages are often sent as two packets (although one containing both data works too); the first one is the length of the zlib-encoded data as 32 Bit Big Endian, then comes the zlib-encoded data.

--- div/decode.js
+++ div/decode.js
@@ -10,9 +10,29 @@ function main() {
 	const args = parser.parseArgs();
 
 	const hex = args.HEX.replace(/\s/, '');
-	const main_buf = Buffer.from(hex, 'hex');
+	let main_buf = Buffer.from(hex, 'hex');
 
-	const response_buf = zlib.gunzipSync(main_buf, {});
+	let response_buf;
+	try {
+		response_buf = zlib.gunzipSync(main_buf, {});
+	} catch(e) {
+		if (e.code !== 'Z_DATA_ERROR') {
+			throw e;
+		}
+
+		const len = main_buf.readInt32BE(0);
+		try {
+			response_buf = zlib.gunzipSync(main_buf.slice(4), {});
+		} catch (_second_err) {
+			throw e;
+		}
+
+		const actual_len = main_buf.length - 4;
+		console.log(
+			`BTP packet, length ${len} ` +
+			((len === actual_len) ? '(correct)' : `(INCORRECT! Actual length is ${actual_len})`) +
+			':');
+	}
 	const response_str = response_buf.toString('utf8');
 	console.log(response_str); // eslint-disable-line no-console
 }
