1eaa3eafa07d4a9bd3f57d86b2db2f0792ba45c7
Philipp Hagemeister <phihag@phihag.de>
2022-06-12 08:49:02 +0200

    assert_snapshot helper function
    Sadly not actually used for our test, because the structure is simple enough!

--- test/tutils.js
+++ test/tutils.js
@@ -13,7 +13,33 @@ const _it = ((typeof it == 'undefined') ?
 	it
 );
 
+const assert = require('assert').strict;
+const path = require('path');
+const {deep_equal} = require('../static/bup/dev/js/utils.js');
+
+
+async function assert_snapshot(dirname, test_name, actual) {
+	assert(actual !== undefined);
+
+	const file_name = path.join(dirname, `${test_name}.snapshot.json`);
+	let expected;
+	try {
+		const contents = await fs.promises.readFile(file_name, 'utf-8');
+		expected = JSON.parse(contents);
+	} catch (e) {
+		expected = `(Error while reading ${file_name}: ${e})`;
+	}
+
+	if (! bup.utils.deep_equal(actual, expected)) {
+		const actual_json = JSON.stringify(actual, undefined, 2);
+		await fs.promises.writeFile(file_name, actual_json);
+		assert.deepStrictEqual(actual, expected);
+	}
+}
+
+
 module.exports = {
+	assert_snapshot,
 	_describe,
 	_it,
 };
