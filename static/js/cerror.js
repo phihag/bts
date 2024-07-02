b78db279b1200db151713bdf36915178b991b56b
tengmel <tim.englich@t-online.de>
2024-07-02 11:52:25 +0200

    Cleanup Errorhandlin in Client

--- static/js/cerror.js
+++ static/js/cerror.js
@@ -3,99 +3,57 @@
 
 var cerror = (function() {
 
-var REPORT_URL = 'https://aufschlagwechsel.de/bupbug/';
-var count = -1;
-var error_list = [];
-var report_enabled = true;
 
-function show(msg) {
-	error_list.unshift(msg);
-	if (typeof uiu !== 'undefined') {
+	var error_list = [];
+	var report_enabled = true;
+
+	function show(msg) {
+		error_list.unshift(msg);
+		if (typeof uiu !== 'undefined') {
+			try {
+				uiu.show_qs('.errors');
+				uiu.text_qs('.errors', error_list.join('\n'));
+			} catch (e) { }
+		}
+	}
+
+
+	function on_error(msg, script_url, line, col, err) {
+		show(msg);
+	}
+
+	function silent(msg) {
+		console.error(msg); // eslint-disable-line no-console
+		on_error(msg, undefined, undefined, undefined, new Error());
+	}
+
+	function net(err)  {
+		silent(err.message);
+	}
+
+	function init() {
+		var report_enabled_json = document.getElementById('bts-data-holder').getAttribute('data-error-reporting');
 		try {
-			uiu.show_qs('.errors');
-			uiu.text_qs('.errors', error_list.join('\n'));
-		} catch (e) { }
+			report_enabled = JSON.parse(report_enabled_json);
+		} catch(e) {
+			silent('Error reporting JSON invalid: ' + report_enabled_json);
+			return;
+		}
+		if (report_enabled === null) {
+			silent('Error reporting not configured');
+			return;
+		}
+		if (report_enabled) {
+			window.onerror = on_error;
+		}
 	}
-}
 
-function get_platform_info() {
 	return {
-		size: document.documentElement.clientWidth + 'x' + document.documentElement.clientHeight,
-		ua: window.navigator.userAgent,
+		init,
+		net,
+		on_error,
+		silent,
 	};
-}
-
-function on_error(msg, script_url, line, col, err) {
-	show(msg);
-
-	if (! report_enabled) {
-		return;
-	}
-
-	count++;
-	if (count > 5) {
-		return;
-	}
-
-	var report = {
-		msg,
-		count,
-		_type: 'bts-error',
-		bts_type: 'client',
-		platform: get_platform_info(),
-	};
-	if (script_url !== undefined) {
-		report.script_url = script_url;
-	}
-	if (line !== undefined) {
-		report.line = line;
-	}
-	if (col !== undefined) {
-		report.col = col;
-	}
-	if (err) {
-		report.stack = err.stack;
-	}
-
-	var report_json = JSON.stringify(report);
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', REPORT_URL, true);
-	xhr.setRequestHeader('Content-type', 'text/plain');  // To be a simple CORS request (avoid CORS preflight)
-	xhr.send(report_json);
-}
-
-function silent(msg) {
-	console.error(msg); // eslint-disable-line no-console
-	on_error(msg, undefined, undefined, undefined, new Error());
-}
-
-function net(err)  {
-	silent(err.message);
-}
-
-function init() {
-	var report_enabled_json = document.getElementById('bts-data-holder').getAttribute('data-error-reporting');
-	try {
-		report_enabled = JSON.parse(report_enabled_json);
-	} catch(e) {
-		silent('Error reporting JSON invalid: ' + report_enabled_json);
-		return;
-	}
-	if (report_enabled === null) {
-		silent('Error reporting not configured');
-		return;
-	}
-	if (report_enabled) {
-		window.onerror = on_error;
-	}
-}
-
-return {
-	init,
-	net,
-	on_error,
-	silent,
-};
 
 })();
 
