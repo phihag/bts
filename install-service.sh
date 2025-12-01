c925e2f4d44d3d9b82846a2aca5e060050a1d8b9
Sascha Heinl <saschad.heinl96@gmail.com>
2025-12-01 23:11:47 +0100

    Add script to install bts as a systemd service

--- install-service.sh
+++ install-service.sh
@@ -7,7 +7,7 @@ if ! command -v node >/dev/null 2>&1; then
 # If not, install it
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
   \. "$HOME/.nvm/nvm.sh"
-  nvm install 24
+  nvm install 18
 fi
 
 # Install BTS to home
