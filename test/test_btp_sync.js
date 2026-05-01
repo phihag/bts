7a708efe62867a81111a5ae6df7da2d62dd9b366
Philipp Hagemeister <phihag@phihag.de>
2026-05-02 00:27:42 +0200

    add a very basic test for league synchronization

--- test/test_btp_sync.js
+++ test/test_btp_sync.js
@@ -5,7 +5,9 @@ const fs = require('fs');
 const path = require('path');
 
 const {init_test} = require('../bts/database');
+const btp_parse = require('../bts/btp_parse');
 const btp_sync = require('../bts/btp_sync');
+require('../bts/compat.js');
 
 const TOURNAMENT_KEY = 'ttest';
 
@@ -18,6 +20,19 @@ describe('btp_sync', () => {
 
 		// await promisify(btp_sync.fetch)()
 		// TODO change umpire name in BTP
-		
+
+	});
+
+	it('league synchronization', async () => {
+		const db = await init_test();
+		db.tournaments.insert({key: TOURNAMENT_KEY, is_team: true});
+		const app = {db};
+
+		const test_file = path.join(__dirname, 'testdata', 'dmm-2026-one-game.json');
+		const contents = await fs.promises.readFile(test_file, 'utf-8');
+		const response = JSON.parse(contents);
+		const btp_state = btp_parse.get_btp_state(response);
+
+		await btp_sync.integrate_btp_state(app, TOURNAMENT_KEY, btp_state);
 	});
 });
